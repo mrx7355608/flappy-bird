dcf0855f17c512e056c463828addc8006b0eaec3
mrx7355608 <mrx7355608@gmail.com>
2023-11-13 20:12:26 +0500

    added value for pipes positions

--- src/movement.cpp
+++ src/movement.cpp
@@ -1,12 +1,13 @@
 #include "../include/Globals.h"
+#include <iostream>
 #include <vector>
 
 std::vector<sf::Sprite> bottomPipes = {};
 std::vector<sf::Sprite> topPipes = {};
 
 int pipeXpos = screenWidth + 30;
-const int pipePosCombos[6][2] = {{-100, 600}, {-150, 550}, {-200, 500},
-                                 {-250, 450}, {-50, 400},  {0, 300}};
+const int pipePosCombos[6][2] = {{-50, 650},  {-100, 600}, {-150, 550},
+                                 {-200, 500}, {-250, 450}, {-150, 400}};
 
 void continuousBaseMovement() {
     const float basePosX = base.getPosition().x;
@@ -25,6 +26,7 @@ int generateRandomNumber() {
 
 void spawnPipes() {
     int randomNumber = generateRandomNumber();
+    std::cout << randomNumber << '\n';
     bottomPipe.setPosition(pipeXpos, pipePosCombos[randomNumber][1]);
     topPipe.setPosition(pipeXpos, pipePosCombos[randomNumber][0]);
     bottomPipes.push_back(bottomPipe);
